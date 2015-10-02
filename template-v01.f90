40d1ff6bce8080061a1f7ada6ac70c4c65110b27
dmr-dj <dmr-dj@users.noreply.github.com>
2015-10-02 14:49:22 +0200

    Initial import of the iLOVECLIM template format for FORTRAN files.

---
+++ template-v01.f90
@@ -0,0 +1,114 @@
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!
+!>      VUA and IPSL/LSCE by the iLOVECLIM / LUDUS coding group / Within the LUDUS code environement
+!
+!       LICENSING TERMS:
+!>      \copyright
+!!      This file is part of [insert sub-component name here, in following Foobar]
+!!      Foobar is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+!!      as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!!
+!!      Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+!!      of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+!!
+!!      You should have received a copy of the GNU General Public License along with Foobar.
+!!      If not, see <http://www.gnu.org/licenses/>.
+!
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+!dmr -- Adding the choice of components through the pre-processing options
+#include 'choixcomposantes.h'
+!dmr -- Adding the choice of components through the pre-processing options
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!      MODULE: [Module_Name here]
+!
+!>     @author  The First Author (tfa)
+!>     @author  The Second Author (tsa)
+!
+!>     @brief This module [Foo_mod] is handling the creation of a particuliar type of fried noodles ...
+!
+!>     @date Creation date: November, 41st, 2999
+!>     @date Last modification: SLastChangedDate$
+!>     @author Last modified by : tfa, tsa
+!
+!>     @version This is svn version: $LastChangedRevision$
+!
+!>     Here add the long_description of the module ...
+!!      more blablas ...
+!!      more blablas ...
+!
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+      module Foo_mod
+
+       use AnotherModule_mod, only: some_variable
+       use AnotherModule_mod, only: some_otherfunction
+
+       implicit none
+       private
+
+       public :: someFunction
+       public :: Foo
+
+       type Foo
+
+       end type Foo
+
+      ! NOTE_avoid_public_variables_if_possible
+
+      contains
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!      ROUTINE: [Routine or Function Name Here]
+!
+!>     @brief This subroutine / function is addind a wonderful knew unused functionality
+!
+!      DESCRIPTION:
+!
+!>     Here add the long_description of the module ...
+!!      more blablas ...
+!!      more blablas ...
+!
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+      function someFunction(inParam, outParam, inOutParam) result(returnValue)
+
+       use AnotherModule_mod, only: some_variable
+       use AnotherModule_mod, only: some_otherfunction
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+! tfa   By reference variables ...
+!>    @param[in]  inParam  The Beaufitul Parameter that does all the input !!
+!>    @param[out] outParam The Beaufitul Parameter that does all the output!!
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+       real, intent(in)    :: inParam
+       real, intent(inout) :: inOutParam
+       real, intent(out)   :: outParam
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+! dmr  Local variables ...
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+       real                :: returnValue
+       real                :: someVariable
+
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!       Main code of the function starts here
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+!>    @bug Description of the stupid sticky bug that we know exist there but is not corrected yet!
+
+      end function someFunction
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!      End of the module here
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+
+end module Foo_mod
+
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
+!      The End of All Things (op. cit.)
+!-----|--1----+----2----+----3----+----4----+----5----+----6----+----7----+----8----+----9----+----0----+----1----+----2----+----3-|
